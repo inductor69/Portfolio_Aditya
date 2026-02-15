450f0696cf9294edfd7fd4786dcec67e4c6b749b
Aditya Kumar <adityakiitdelhi@gmail.com>
2026-02-15 20:56:43 +0530

    refactor: update default sans font to GeistMono in fonts and global styles

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,7 +1,6 @@
 import { GeistMono } from "geist/font/mono"
 import { GeistPixelSquare } from "geist/font/pixel"
-import { GeistSans } from "geist/font/sans"
 
-export const fontSans = GeistSans
+export const fontSans = GeistMono
 export const fontMono = GeistMono
 export const fontPixelSquare = GeistPixelSquare
